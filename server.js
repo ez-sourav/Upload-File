dec3d8856f4b04f8f1e0acb742ffd282c0b4cc26
ez-sourav <souravb2003@gmail.com>
2025-12-25 12:02:23 +0530

    commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const app = require('./app')
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port", process.env.PORT);
+});
