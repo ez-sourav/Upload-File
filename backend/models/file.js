8ec46ab84194d2d4d5e9451f38d2caae349e17df
ez-sourav <souravb2003@gmail.com>
2025-12-29 04:20:16 +0530

    add cloudinary

--- backend/models/file.js
+++ backend/models/file.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-const fileSchema = new mongoose.Schema({
-  originalName: String,
-  fileName: String,
-  fileType: String,
-  size: Number,
-  path: String
-}, { timestamps: true });
+
+const fileSchema = new mongoose.Schema(
+  {
+    originalName: String,
+    fileName: String,
+    fileType: String,
+    size: Number,
+    fileUrl: String,   // Cloudinary URL
+    publicId: String,  // Cloudinary ID (for delete)
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("File", fileSchema);
