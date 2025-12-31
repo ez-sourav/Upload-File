f647da0d314a07f07b9acbfff1db57e35150eeed
ez-sourav <souravb2003@gmail.com>
2026-01-01 02:24:18 +0530

    improve Ui

--- frontend/src/components/UploadFile.jsx
+++ frontend/src/components/UploadFile.jsx
@@ -3,11 +3,30 @@ import api from "../api/api";
 import UploadZone from "./UploadZone";
 import toast from "react-hot-toast";
 
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+
 export default function UploadFile({ onUploadSuccess }) {
+
+  const { user } = useContext(AuthContext);
+const navigate = useNavigate();
+
+
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const uploadFiles = async () => {
+
+    // 🔐 AUTH GUARD (PREVIEW MODE)
+  if (!user) {
+    toast.error("Please login to upload files");
+    navigate("/login");
+    return;
+  }
+
+
     if (selectedFiles.length === 0) {
       toast.error("Please select at least one file");
       return;
@@ -47,6 +66,13 @@ export default function UploadFile({ onUploadSuccess }) {
         Upload Files
       </h3>
 
+      {!user && (
+  <p className="mb-3 text-sm text-orange-600 bg-orange-50 px-3 py-2 rounded">
+    You are in preview mode. Login to upload files.
+  </p>
+)}
+
+
       {/* Upload Zone */}
       <UploadZone
         selectedFiles={selectedFiles}
@@ -59,14 +85,14 @@ export default function UploadFile({ onUploadSuccess }) {
         <div className="mt-4 flex justify-end">
           <button
             onClick={uploadFiles}
-            disabled={loading}
+            disabled={loading || !user}
             className="
-              w-full sm:w-auto
-              bg-blue-600 text-white
-              px-4 md:px-5 py-2
-              rounded-lg text-sm
-              disabled:opacity-50 hover:cursor-pointer
-            "
+  w-full sm:w-auto
+  bg-blue-600 text-white
+  px-4 md:px-5 py-2
+  rounded-lg text-sm
+  disabled:opacity-50 disabled:cursor-not-allowed
+"
           >
             {loading
               ? "Uploading..."
