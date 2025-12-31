9f8acf6170d2937dad9c2b54a338a50ca8a5014e
ez-sourav <souravb2003@gmail.com>
2026-01-01 02:26:59 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Toaster position="top-right" />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster position="top-right" />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
+
