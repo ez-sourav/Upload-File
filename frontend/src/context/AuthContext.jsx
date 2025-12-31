4d712a2ca3ca365552dd001c420f6ef09e5ab30e
ez-sourav <souravb2003@gmail.com>
2026-01-01 02:25:14 +0530

    Auth Contex

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(
+    localStorage.getItem("token")
+  );
+  const [loading, setLoading] = useState(true);
+
+  // Restore auth on refresh
+  useEffect(() => {
+    if (token) {
+      const savedUser = localStorage.getItem("user");
+      if (savedUser) {
+        setUser(JSON.parse(savedUser));
+      }
+    }
+    setLoading(false);
+  }, [token]);
+
+  const login = (data) => {
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+    setToken(data.token);
+    setUser(data.user);
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    setUser(null);
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, token, login, logout, loading }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
