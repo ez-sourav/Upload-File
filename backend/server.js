2e5f36da3e75b426e8de374201d7d2357f776760
ez-sourav <souravb2003@gmail.com>
2026-01-02 00:18:25 +0530

    modify

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port", process.env.PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
