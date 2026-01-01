fd9f8cc4af122a25ade758592ee730f4a26d768c
ez-sourav <souravb2003@gmail.com>
2026-01-02 00:18:15 +0530

    modify

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,13 @@ const authRoutes = require("./routes/auth");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
